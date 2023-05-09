2f3a0536de9c54fb655670fccb393d5450556878
Carlos <carlosdanieltc@gmail.com>
2023-05-09 17:01:02 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      height: {
+        '88' : '22rem',
+        '104': '26rem'
+      },
       colors: {
-        primaryDark: '#282c34',
+        primaryDark: '#282c34'
       },
       fontFamily: {
         
@@ -12,7 +16,11 @@ module.exports = {
       borderRadius: {
         borderCustom: '0 700px 700px 0'
       }
-    },
+    }
+    // screens: {
+    //   '3xl': '1796px'
+    //   // => @media (min-width: 1796px) { ... }
+    // }
   },
   plugins: [],
 }
