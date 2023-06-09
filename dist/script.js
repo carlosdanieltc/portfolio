95790402dd13a90385477b6da56ac8ddaa26b6e1
Carlos <carlosdanieltc@gmail.com>
2023-06-09 22:15:31 +0200

    Cambios generales

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,8 @@
 //MENU VARIABLES
 
 const menuButton = document.querySelector('#menu-button');
+const darkModeButton = document.querySelector('#dark-mode-button');
+const html = document.querySelector('html');
 const menuItems = document.querySelector('#menu');
 const links = document.querySelectorAll('#menu a');
 
@@ -8,4 +10,10 @@ const links = document.querySelectorAll('#menu a');
 
 menuButton.addEventListener('click', () => {
     menuItems.classList.toggle('hidden');
+    menuButton.classList.toggle('text-white');
+});
+
+//DARK MODE
+darkModeButton.addEventListener('click', () => {
+    html.classList.toggle('dark');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,19 @@ module.exports = {
   theme: {
     extend: {
       height: {
-        '88' : '22rem',
+        '88': '22rem',
         '104': '26rem'
       },
       colors: {
         primaryDark: '#282c34'
       },
       fontFamily: {
-        
+
       },
       borderRadius: {
         borderCustom: '0 700px 700px 0'
-      }
+      },
     }
-    // screens: {
-    //   '3xl': '1796px'
-    //   // => @media (min-width: 1796px) { ... }
-    // }
   },
   plugins: [],
 }
