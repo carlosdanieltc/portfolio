84554780335ed35c195979eed1545e59e4b92b82
Carlos <carlosdanieltc@gmail.com>
2023-06-12 17:29:11 +0200

    Ajustes darkMode

--- docs/script.js
+++ docs/script.js
@@ -18,4 +18,13 @@ menuButton.addEventListener('click', () => {
 
 darkModeButton.addEventListener('click', () => {
     htmlElement.classList.toggle('dark');
-  });
+});
+
+// Detectar si el dispositivo está en modo noche
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Agregar la clase 'dark' al elemento <html>
+    htmlElement.classList.add('dark');
+  } else {
+    // Si no está en modo noche, asegúrate de que la clase 'dark' no esté presente
+    htmlElement.classList.remove('dark');
+  }
