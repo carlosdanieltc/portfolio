22cbf4bb4edbd2dbd4f08f9848699799b064b48f
Carlos <carlosdanieltc@gmail.com>
2023-06-12 16:47:59 +0200

    Implementado Dark Mode

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,21 @@
 //MENU VARIABLES
 
-const menuButton = document.querySelector('#menu-button');
-const darkModeButton = document.querySelector('#dark-mode-button');
-const html = document.querySelector('html');
-const menuItems = document.querySelector('#menu');
-const links = document.querySelectorAll('#menu a');
+const menuButton = document.getElementById('menu-button');
+const menuItems = document.getElementById('menu');
+const darkModeButton = document.getElementById('dark-mode-button');
+const htmlElement = document.querySelector('html');
 
 //ABRE MENU
-
 menuButton.addEventListener('click', () => {
     menuItems.classList.toggle('hidden');
     menuButton.classList.toggle('text-white');
 });
 
 //DARK MODE
+
+// Deshabilitar el modo oscuro al cargar la página
+//htmlElement.classList.remove('dark');
+
 darkModeButton.addEventListener('click', () => {
-    html.classList.toggle('dark');
-});
+    htmlElement.classList.toggle('dark');
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     }
   },
+  darkMode: 'class',
   plugins: [],
 }
 
