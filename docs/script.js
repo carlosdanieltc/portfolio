7c63b0d173ae1e8d3584460a3dd81b100ea6be07
Carlos <carlosdanieltc@gmail.com>
2023-06-13 15:13:32 +0200

    Ajustes generales

--- docs/script.js
+++ docs/script.js
@@ -1,30 +1,34 @@
 //MENU VARIABLES
 
 const menuButton = document.getElementById('menu-button');
-const menuItems = document.getElementById('menu');
+const menuItems = document.getElementById('menu-list');
 const darkModeButton = document.getElementById('dark-mode-button');
 const htmlElement = document.querySelector('html');
 
 //ABRE MENU
 menuButton.addEventListener('click', () => {
-    menuItems.classList.toggle('hidden');
-    menuButton.classList.toggle('text-white');
+  menuItems.classList.toggle('hidden');
+  menuButton.classList.toggle('text-white');
 });
 
 //DARK MODE
-
-// Deshabilitar el modo oscuro al cargar la página
-//htmlElement.classList.remove('dark');
-
 darkModeButton.addEventListener('click', () => {
-    htmlElement.classList.toggle('dark');
+  htmlElement.classList.toggle('dark');
 });
 
-// Detectar si el dispositivo está en modo noche
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+// Función para verificar el modo noche y actualizar la clase 'dark'
+function checkDarkMode(event) {
+  if (event.matches) {
     // Agregar la clase 'dark' al elemento <html>
     htmlElement.classList.add('dark');
   } else {
     // Si no está en modo noche, asegúrate de que la clase 'dark' no esté presente
     htmlElement.classList.remove('dark');
   }
+}
+
+// Verificar el modo noche al cargar la página
+checkDarkMode(window.matchMedia('(prefers-color-scheme: dark)'));
+
+// Escuchar cambios en el modo noche
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', checkDarkMode);
